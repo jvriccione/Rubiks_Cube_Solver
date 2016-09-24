0b7e7762a4f5deea72d80210aa9f31e9d36cc48d
John Vincent Riccione <jvriccione@johns-mbp.lfytina2.metronetinc.com>
2016-09-24 17:13:30 -0400

    Initial Commit

---
+++ Rubiks_Cube_Solver/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rubiks_Cube_Solver
+//
+//  Created by John Vincent Riccione on 9/24/16.
+//  Copyright © 2016 John Vincent Riccione. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
