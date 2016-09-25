70bc52b493f6df35bf4e3876e8eb58c6f3329298
John Vincent Riccione <jvriccione@Johns-MBP.lfytina2.metronetinc.com>
2016-09-24 23:41:20 -0400

    cropping images, setting up initial arrays, etc

---
+++ Rubiks_Cube_Solver/Block.swift
@@ -0,0 +1,22 @@
+//
+//  Block.swift
+//  Rubiks_Cube_Solver
+//
+//  Created by John Vincent Riccione on 9/24/16.
+//  Copyright © 2016 John Vincent Riccione. All rights reserved.
+//
+
+import Foundation
+
+class Block {
+    
+    /*
+    var red = 0
+    var green = 0
+    var blue = 0
+    
+    init(red : , green, blue) {
+        <#statements#>
+    }
+ */
+}
--- Rubiks_Cube_Solver/ViewController.swift
+++ Rubiks_Cube_Solver/ViewController.swift
@@ -12,24 +12,27 @@ import ChameleonFramework
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    //    let urlImg = "https://c1.staticflickr.com/5/4034/4544827697_6f73866999_b.jpg"
-    
     @IBOutlet weak var imageView: UIImageView!
     var newMedia: Bool?
     let imagePicker = UIImagePickerController()
-    var imgColor : UIColor = UIColor()
-    var jpgPath : NSString = NSHomeDirectory() as NSString
+    var colorArr1 : NSMutableArray = []     //what positions are this color
+    var colorArr2 : NSMutableArray = []
+    var colorArr3 : NSMutableArray = []
+    var colorArr4 : NSMutableArray = []
+    var colorArr5 : NSMutableArray = []
+    var colorArr6 : NSMutableArray = []
+    var rgbArr = [[CGFloat]]()
+    var tolerance : CGFloat = 0.15
+    
+    var isFirstBlock = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
     }
     
     @IBAction func useCamera(_ sender: AnyObject) {
-        
+       
         if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
-            
             
             imagePicker.delegate = self
             imagePicker.sourceType = UIImagePickerControllerSourceType.camera
@@ -40,27 +43,76 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             
             newMedia = true
         }
-        
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [AnyHashable: Any]!) {
-        imageView.image = image
         
         // Divide image into 3 x 3 grid
+        let blockSide = image.size.width/3
+        var imgColor : UIColor = UIColor()
+        var ct = 1
+        let offset = blockSide/4
+        
+        for m in 0...2 {
+            for n in 0...2 {
+                let x = CGFloat(n) * blockSide + offset
+                let y = CGFloat(m) * blockSide + offset
+                let subImg = cropImage(original: image, x: x, y: y)
+                print("Image\(ct) x:\(n), y\(m)")
+                
+                if n == 1 && m == 2 {
+                    imageView.image = subImg
+                }
+                
+                
+                imgColor = UIColor(averageColorFrom: subImg)
+                print(imgColor)
+                
+                let redVal = imgColor.cgColor.components![0]
+                let greenVal = imgColor.cgColor.components![1]
+                let blueVal = imgColor.cgColor.components![2]
+                
+                
+                var minDelta : CGFloat = 1
+                var currentColorIndex : Int?
+                
+                for i in 0...((rgbArr.capacity/3) - 1) {
+                    let currentDelta = abs(redVal - rgbArr[i][0])
+                    if currentDelta < minDelta {
+                        minDelta = currentDelta
+                        currentColorIndex = i
+                    }
+                }
+                
+                rgbArr[currentColorIndex!][0] = (rgbArr[currentColorIndex!][0] + redVal)/2
+                
+                
+                
+                
+                if isFirstBlock {
+                    colorArr1[0] = ct
+                    
+                    rgbArr[0][0] = redVal
+                    rgbArr[0][1] = greenVal
+                    rgbArr[0][2] = blueVal
+                    
+                    isFirstBlock = false
+                }
+                
+                
+                
+                ct += 1
+            }
+        }
         
         
-        
-        imgColor = UIColor(averageColorFrom: image)
-        print(imgColor)
-        
-        
-        //        currentImg = UIImageJPEGRepresentation(image, 0.8)!
-        //        currentImg.write(to: jpgPath)
-        //let imageString = jpeg?.base64EncodedString()
-        //currentImg = NSURL(string: imageString!)!
+        //imageView.image = image
+        //print(image.size.width)
+        //print(image.size.height)
+
         
         
-        //        findMainColorOfImageWithURL()
+
         self.dismiss(animated: true, completion: nil);
     }
     
@@ -68,35 +120,17 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         self.dismiss(animated: true, completion: nil)
     }
     
-    /*    func findMainColorOfImageWithURL() {
-     
-     
-     let headers: HTTPHeaders = [
-     "X-Mashape-Key": "7y2as4tWCBmshfUT7x8ymCW1h37ip1fnQ7jjsnMpCwqgvOsstZ",
-     "Accept": "application/json"
-     ]
-     let parameters: Parameters = [
-     "image": currentImg,
-     "palette": "simple",
-     "sort":"weight"
-     ]
-     
-     Alamofire.request("https://apicloud-colortag.p.mashape.com/tag-file.json", parameters: parameters, headers: headers).responseJSON { response in
-     
-     print(response.debugDescription)
-     
-     if let result = response.result.value {
-     if let resultDict = result as? NSDictionary {
-     if let colorsArr = resultDict["tags"] as? [[String : AnyObject]] {
-     
-     let firstColorDict = colorsArr[0]
-     if let firstColor = firstColorDict["label"] as? String {
-     print("Main Color Identified: \(firstColor)")
-     }
-     }
-     }
-     }
-     }
-     }
-     */
+    func cropImage(original: UIImage, x: CGFloat, y: CGFloat) -> UIImage {
+        
+        let width = original.size.width/6
+        let height = width
+        let cgImg = original.cgImage
+        
+        let cropRect = CGRect(x: x, y: y, width: width, height: height)
+        let croppedCGImage = cgImg!.cropping(to: cropRect)
+        
+        let finalImg = UIImage(cgImage: croppedCGImage!)
+        
+        return finalImg
+    }
 }
